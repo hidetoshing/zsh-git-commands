3d2930df7f98c54e4bd5dfaa7d1a025b254cfaad
hidetoshing <hidetoshing@gmail.com>
2016-12-01 20:03:57 +0900

    :art: change internal function name

--- git-commands.plugin.zsh
+++ git-commands.plugin.zsh
@@ -2,34 +2,34 @@
 
 #####
 # get hash code from git log
-function git-hash()
+function git-select-hash()
 {
     git log --oneline --branches | fzy --prompt "GIT HASH > " | awk '{print $1}'
 }
-alias -g HASH='$(git-hash)'
+alias -g HASH='$(git-select-hash)'
 
 #####
 # get modified file
-function git-modified()
+function git-select-modified()
 {
     git status --short | fzy --prompt "GIT MODIFIED FILE > " | awk '{print $2}'
 }
-alias -g MODIFIES='$(git-modified)'
+alias -g MODIFIES='$(git-select-modified)'
 
 #####
 # get file changed from master
-function git-changed()
+function git-select-changed()
 {
     BRANCH=${1:-`git rev-parse --abbrev-ref HEAD`}
     git diff --name-only master...${BRANCH} | fzy --prompt "GIT CHANGED FILE > "
 }
-alias -g CHANGED='$(git-changed)'
+alias -g CHANGED='$(git-select-changed)'
 
 #####
 # get branch name
-function git-branch()
+function git-select-branch()
 {
     git branch | fzy --prompt "GIT BRANCH > " | head -n 3 | sed -e "s/^\*\s*//g"
 }
-alias -g BRANCH='$(git-branch)'
+alias -g BRANCH='$(git-select-branch)'
 
